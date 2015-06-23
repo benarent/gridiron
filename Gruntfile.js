c3917e20addedf6c0ee1e19c0f7f857a4d12b365
Ben Arent <info@benarent.co.uk>
2015-06-23 11:31:53 -0700

    Basic bolierplate and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+   // Re-usable filesystem paths (these shouldn't be modified)
+    paths: {
+      src:        'src',
+      src_img:    'src/img',
+      dist:       'dist',
+      dist_img:   'dist/img'
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'src',
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      options: {
+      livereload: true,
+      },
+      html: {
+        files: ['src/index.html'],
+        tasks: ['htmlhint']
+      },
+      css: {
+        files: [
+          '<%= paths.src %>/css/*.css'
+        ],
+        tasks: ['default']
+      },
+      js: {
+        files: [
+          'assets/js/*.js',
+          'Gruntfile.js'
+        ],
+
+      }
+    },
+   
+  });
+
+  // Load the Grunt plugins.
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Register the default tasks.
+  grunt.registerTask('default', ['connect', 'watch']);
+};
